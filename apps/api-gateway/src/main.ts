ede4c3b80f8cb4178beea5af9d149811eefdf18d
arghadlufy <arghadlufy@gmail.com>
2025-06-01 10:55:02 +0530

    initial cors, cookie parser, rate limit and proxy setup

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,14 +1,36 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from "express";
-import * as path from "path";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+// import swaggerUi from "swagger-ui-express";
+// import axios from "axios";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+app.use(cookieParser());
+app.set("trust proxy", 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
+app.use("/api/auth", proxy("http://localhost:6001")); // auth service
 
 app.get("/api", (req, res) => {
   res.send({ message: "Welcome to api-gateway!" });
